d09c2d0f8b551cbc74a172fd788553f87f853ea7
andrejz2010 <zacharovandrej1@gmail.com>
2023-09-25 14:35:35 +0100

    Added check if there is a piece in front of pawn, to check if it can move or not.

--- app/src/main/java/chessington/model/pieces/Pawn.java
+++ app/src/main/java/chessington/model/pieces/Pawn.java
@@ -5,6 +5,8 @@ import chessington.model.Coordinates;
 import chessington.model.Move;
 import chessington.model.PlayerColour;
 
+
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,16 +20,47 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        ArrayList<Move> allowedMoves= new ArrayList<Move>();
+        ArrayList<Move> allowedMoves= new ArrayList<>();
+        Coordinates PieceInFrontOfWhite=new Coordinates(from.getRow()-1, from.getCol());
+        Coordinates PieceInFrontOfTwoSquareWhite=new Coordinates(from.getRow()-2, from.getCol());
+        Coordinates PieceInFrontOfBlack=new Coordinates(from.getRow()+1, from.getCol());
+        Coordinates PieceInFrontOfTwoSquareBlack=new Coordinates(from.getRow()+2, from.getCol());
+
 
         if (this.colour==WHITE) {
-            Coordinates to1 = new Coordinates(from.getRow()-1, from.getCol());
-            Move allowedMove1=new Move(from, to1);
-            allowedMoves.add(allowedMove1);
-        } else if (this.colour==BLACK){
-            Coordinates to2 = new Coordinates(from.getRow()+1, from.getCol());
-            Move allowedMove2=new Move(from, to2);
-            allowedMoves.add(allowedMove2);
+            Piece pieceInFront = board.get(PieceInFrontOfWhite);
+            Piece pieceInFrontTwoSquare = board.get(PieceInFrontOfTwoSquareWhite);
+
+                if (from.getRow() == 6) {
+
+                    Coordinates to1 = new Coordinates(from.getRow() - 2, from.getCol());
+                    if (pieceInFrontTwoSquare == null) {
+                        Move allowedMove1 = new Move(from, to1);
+                        allowedMoves.add(allowedMove1);
+                    }
+                }
+            if (pieceInFront == null) {
+                Coordinates to1 = new Coordinates(from.getRow() - 1, from.getCol());
+                Move allowedMove1 = new Move(from, to1);
+                allowedMoves.add(allowedMove1);
+            }
+            } else if (this.colour == BLACK) {
+            Piece pieceInFront = board.get(PieceInFrontOfBlack);
+            Piece pieceInFrontTwoSquare = board.get(PieceInFrontOfTwoSquareBlack);
+
+
+
+                if (from.getRow() == 1) {
+                    Coordinates to1 = new Coordinates(from.getRow() + 2, from.getCol());
+                    if (pieceInFrontTwoSquare == null){
+                    Move allowedMove1 = new Move(from, to1);
+                    allowedMoves.add(allowedMove1);}
+                }
+                if(pieceInFront==null) {
+                    Coordinates to2 = new Coordinates(from.getRow() + 1, from.getCol());
+                    Move allowedMove2 = new Move(from, to2);
+                    allowedMoves.add(allowedMove2);
+                }
 
         }
 
